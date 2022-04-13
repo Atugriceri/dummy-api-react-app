92e5cf74129b6cfb9f9537a519ddeb58e49a8b0b
Alper Tugriçeri <alpertugriceri@gmail.com>
2022-04-14 00:57:12 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import PostList from './Pages/PostList'
+import UserList from './Pages/UserList'
+import UserDetail from './Pages/UserDetail'
+import Container from './Components/Container'
+import NavigationBar from './Components/NavigationBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavigationBar />
+      <Container>
+        <Routes>
+          <Route exact path="/" element={<Navigate to="/posts" />} />
+          <Route path="/posts" element={<PostList />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/users/:user_id" element={<UserDetail />} />
+        </Routes>
+      </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
